741be9b3de66f0251b6893640de1040e77d0c4df
Abhi Agarwal <abhiaagarwal01@gmail.com>
2025-09-13 02:12:37 -0400

    First set
    Signed-off-by: Abhi Agarwal <abhiaagarwal01@gmail.com>

---
+++ dotfiles/dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+set fish_greeting ""
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    atuin init fish | source
+    zoxide init fish --cmd cd | source
+    starship init fish | source
+end
